bbe207e15da0479426cffac20bdbf7bafb6078c2
B. Madhu <madhu_lists@yahoo.com>
2023-11-26 12:04:14 -0500

    adapted to Postgres

--- src/clj/votingbuddy/routes/services.clj
+++ src/clj/votingbuddy/routes/services.clj
@@ -14,7 +14,8 @@
    [votingbuddy.middleware :as middleware]
    [ring.util.http-response :as response]
    [votingbuddy.middleware.formats :as formats]
-   [votingbuddy.auth :as auth]))
+   [votingbuddy.auth :as auth]
+   [spec-tools.data-spec :as ds]))
 
 (defn service-routes []
   ["/api"
@@ -29,6 +30,78 @@
     :muuntaja formats/instance
     :coercion spec-coercion/coercion
     :swagger {:id ::api}}
+   ["/session"
+    {:get
+     {:responses
+      {200
+      {:body
+       {:session
+        {:identity (ds/maybe {:login string?
+                              :created_at inst?})}}}}
+    :handler
+    (fn [{{:keys [identity]} :session}]
+      (response/ok {:session
+                    {:identity
+                     (not-empty
+                      (select-keys
+                       identity [:login :created_at]))}}))}}]
+   ["/login"
+    {:post {:parameters
+            {:body
+             {:login string?
+              :password string?}}
+            :responses
+            {200
+             {:body
+              {:identity
+               {:login string?
+                ;; :first_name nil?
+                ;; :last_name nil?
+                ;; :admin nil?
+                ;; :last_login nil?
+                ;; :is_active nil?
+                :created_at inst?}}}
+             401
+             {:body
+              {:message string?}}}
+            :handler
+            (fn [{{{:keys [login password]} :body} :parameters session :session}]
+              (if-some [user (auth/authenticate-user login password)]
+                ;; (do
+                  ;; (println "--BEGIN--")
+                  ;; (println session)
+                  ;; (println "--END--")
+                  ;; (println "Should login user!")
+
+                (-> (response/ok {:identity user})
+                    (assoc :session (assoc session :identity user)))
+                (response/unauthorized {:message "Incorrect login or password."})))}}]
+   ["/register"
+    {:post {:parameters
+            {:body
+             {:login string?
+              :password string?
+              :confirm string?}}
+            :responses
+            {200
+             {:body {:message string?}}
+             400
+             {:body {:message string?}}
+             409
+             {:body {:message string?}}}
+            :handler
+            (fn [{{{:keys [login password confirm]} :body} :parameters}]
+              (if-not (= password confirm)
+                (response/bad-request {:message "Password & Confirm do not match."})
+                (try (auth/create-user! login password)
+                     (response/ok {:message "User registration successful. Please login."})
+                     (catch clojure.lang.ExceptionInfo e (if (= (:votingbuddy/error-id (ex-data e))
+                                                                ::auth/duplicate-user)
+                                                           (response/conflict {:message "Registration failed! User with login already exists!"})
+                                                           (throw e))))))}}]
+   ["/logout"
+    {:post {:handler (fn [_] (-> (response/ok) (assoc :session nil)))}}]
+
    ["" {:no-doc true}
     ["/swagger.json"
      {:get (swagger/create-swagger-handler)}]
